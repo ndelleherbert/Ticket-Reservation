333cf9180bc72d6b03eecf1a06133a6714abc6a7
benndip <obentabiayuk1@gmail.com>
2020-06-05 01:05:47 +0100

    I just resolved somthing fatal

--- src/pages/SIGN_UP/signup.js
+++
@@ -1,9 +0,0 @@
-// get the modal
-var modal= document.getElementById('id01');
-
-/* windows should close when user clicks anywehere outsided the model */
-window.onclick = function(event){
-    if(event.target==modal){
-        modal.style.display="none";
-    }
-}
